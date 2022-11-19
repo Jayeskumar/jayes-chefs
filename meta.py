46470309c9f0fd51d21c608c8568b701f029ecb9
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-11-19 10:47:13 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -9,21 +9,21 @@ SIDEBAR_INFO = """
 </div>
 """
 CHEF_INFO = """
-<h2 class="font-title">Welcome to our lovely restaurant! </h2>
+<h2 class="font-title">Welcome to  Jayes's Restaurant! </h2>
 <p class="strong font-body">
-<span class="d-block extra-info">(We are at your service with two of the best chefs in the world: Chef Scheherazade, 
-Chef Giovanni. Scheherazade is known for being more creative whereas Giovanni is more meticulous.)</span>
+<span class="d-block extra-info">(We are at your service with two of the best chefs in the world: Chef Jayes, 
+Chef Pradhap. Jayes is known for being more creative whereas Pradhap is more meticulous.)</span>
 </p>
 """.strip()
 PROMPT_BOX = "Add custom ingredients here (separated by `,`): "
 STORY = """
 <div class="story-box font-body">
 <p>
-Hello everyone 👋, I am <strong>Chef Transformer</strong>, 
+Hello everyone 👋, I am <strong>Chef Dhamu</strong>, 
 the owner of this restaurant. I was made by a group of <a href="https://huggingface.co/flax-community/t5-recipe-generation#team-members">NLP Engineers</a> to train my two prodigy recipe creators: <strong>Chef Scheherazade</strong> and <strong>Chef Giovanni</strong>. 
 Both of my students participated in my rigorous culinary program, <a href="https://huggingface.co/flax-community/t5-recipe-generation">T5 fine-tuning</a>, 
 to learn how to prepare exquisite cuisines from a wide variety of ingredients. 
-I've never been more proud of my students -- both can produce exceptional dishes but I regard Scheherazade as being <em>creative</em> while Giovanni is <em>meticulous</em>. 
+I've never been more proud of my students -- both can produce exceptional dishes but I regard Jayes as being <em>creative</em> while Pradhap is <em>meticulous</em>. 
 If you give each of them the same ingredients, they'll usually come up with something different. <br /><br />
 At the start of the program the chefs read cookbooks containing thousands of recipes of varying difficulties and from cultures all over the world. 
 The NLP engineers helped guide the learning process so that the chefs could actually learn which ingredients work well together rather than just memorize recipes. 
