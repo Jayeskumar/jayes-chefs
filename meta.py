45d42e6d8afff81be332b9be508630f897a05d92
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-11-19 10:51:56 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -2,7 +2,7 @@ HEADER_INFO = """""".strip()
 SIDEBAR_INFO = """
 <div class="contributors font-body text-bold">
 <a class="contributor comma" href="https://huggingface.co/m3hrdadfi">Jayeskumar M</a>
-<a class="contributor comma" href="https://huggingface.co/dk-crazydiv">Akila V</a>
+
 
 </div>
 """
